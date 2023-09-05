21a11bd8cc5c8a04f0c063297375f5ca6e79bbc5
Nathaniel Valla (RIT Student) <njv3788@rit.edu>
2023-09-05 15:47:51 -0400

    finished Lab1

---
+++ DE1_SoC_Lab1/Part2/src/SW/nios_software/obj/default/lights_main.d
@@ -0,0 +1,6 @@
+obj/default/lights_main.o: lights_main.c system.h \
+ S:/Documents/gitrepos/CPET561/DE1_SoC_Lab1/part2/src/SW/nios_software_bsp/linker.h
+
+system.h:
+
+S:/Documents/gitrepos/CPET561/DE1_SoC_Lab1/part2/src/SW/nios_software_bsp/linker.h:
