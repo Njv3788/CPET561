20dd18c0a0047f1d5a95d2b812976f7906fb53de
Nate Valla <77633870+Njv3788@users.noreply.github.com>
2023-09-05 21:39:36 -0400

    demo2 started

--- DE1_SoC_Demo2/src/HW/assembly_demo.vhd
+++ DE1_SoC_Demo2/src/HW/assembly_demo.vhd
@@ -11,20 +11,18 @@ use ieee.std_logic_arith.all;
 ENTITY assembly_demo is
   port (
     
-
     ----- CLOCK -----
     CLOCK_50 : in std_logic;
-	 
+  
     ----- SW -----
     SW : in  std_logic_vector(9 downto 0);
-
+  
     ----- KEY -----
     KEY : in std_logic_vector(3 downto 0); --for reset
-	 
-	 ----- LED -----
+  
+  ----- LED -----
     LEDR : out  std_logic_vector(9 downto 0)  --for heartbeat
-
-    
+  
   );
 end entity assembly_demo;
 
@@ -39,13 +37,13 @@ architecture rtl of assembly_demo is
   
   -- nios_system component
   component nios_system is
-		port (
-			clk_clk         : in  std_logic                    := 'X';             -- clk
-			leds_export     : out std_logic_vector(7 downto 0);                    -- export
-			reset_reset_n   : in  std_logic                    := 'X';             -- reset_n
-			switches_export : in  std_logic_vector(7 downto 0) := (others => 'X')  -- export
-		);
-	end component nios_system;
+    port (
+      clk_clk         : in  std_logic                    := 'X';             -- clk
+      leds_export     : out std_logic_vector(7 downto 0);                    -- export
+      reset_reset_n   : in  std_logic                    := 'X';             -- reset_n
+      switches_export : in  std_logic_vector(7 downto 0) := (others => 'X')  -- export
+    );
+  end component nios_system;
   
   
 begin
@@ -77,11 +75,11 @@ begin
   
   --Port map the nios_system
   u0 : component nios_system
-		port map (
-			clk_clk       => CLOCK_50,       --   clk.clk
-			reset_reset_n => reset_n,  -- reset.reset_n
-			leds_export     => LEDR(7 downto 0),     --     leds.export
-			switches_export => SW(7 downto 0)  -- switches.export
-		);
+    port map (
+      clk_clk         => CLOCK_50,          -- clk.clk
+      reset_reset_n   => reset_n,           -- reset.reset_n
+      leds_export     => LEDR(7 downto 0),  -- leds.export
+      switches_export => SW(7 downto 0)     -- switches.export
+    );
     
 end architecture rtl;
