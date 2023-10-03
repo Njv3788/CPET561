e2cbfb3a0ec79d3b878c1dc68431ea57d6e7317a
Nathaniel Valla (RIT Student) <njv3788@rit.edu>
2023-10-03 16:25:44 -0400

    lab 5 start

---
+++ DE1_SoC_Demo5/src/SW/nios_software/obj/default/custom_component_demo.d
@@ -0,0 +1,18 @@
+obj/default/custom_component_demo.o: custom_component_demo.c system.h \
+ C:/gitrepos/CPET561/DE1_SoC_Demo4/src/SW/nios_software_bsp/linker.h \
+ c:\gitrepos\cpet561\de1_soc_demo4\src\sw\nios_software_bsp\hal\inc\sys\alt_irq.h \
+ c:\gitrepos\cpet561\de1_soc_demo4\src\sw\nios_software_bsp\hal\inc\nios2.h \
+ c:\gitrepos\cpet561\de1_soc_demo4\src\sw\nios_software_bsp\hal\inc\alt_types.h \
+ C:/gitrepos/CPET561/DE1_SoC_Demo4/src/SW/nios_software_bsp/system.h
+
+system.h:
+
+C:/gitrepos/CPET561/DE1_SoC_Demo4/src/SW/nios_software_bsp/linker.h:
+
+c:\gitrepos\cpet561\de1_soc_demo4\src\sw\nios_software_bsp\hal\inc\sys\alt_irq.h:
+
+c:\gitrepos\cpet561\de1_soc_demo4\src\sw\nios_software_bsp\hal\inc\nios2.h:
+
+c:\gitrepos\cpet561\de1_soc_demo4\src\sw\nios_software_bsp\hal\inc\alt_types.h:
+
+C:/gitrepos/CPET561/DE1_SoC_Demo4/src/SW/nios_software_bsp/system.h:
